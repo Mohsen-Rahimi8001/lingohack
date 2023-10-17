a0d4c39f0875c5181d472527c1e4bc52ba679202
Mohsen-Rahimi8001 <mohsenrahimi8001@gmail.com>
2023-10-17 12:08:35 +0330

    init project & add models

---
+++ database/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import (
+    Boolean,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Float
+)
+from sqlalchemy.orm import relationship
+
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    tables = relationship("Table", back_populates="writer")
+
+
+class Table(Base):
+    __tablename__ = "tables"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    difficulty = Column(Float, default=0)
+
+    writer_id = Column(Integer, ForeignKey("users.id"))
+
+    phrases = relationship("Phrase", back_populates="table")
+
+
+class Phrase(Base):
+    __tablename__ = "phrases"
+
+    id = Column(Integer, primary_key=True, index=True)
+    phrase = Column(String, index=True)
+    meaning = Column(String, index=True)
+    description = Column(String, index=True)
+    difficulty = Column(Float, default=0)
+
+    table_id = Column(Integer, ForeignKey("tables.id"))
+
+    table = relationship("Table", ForeignKey("tables.table"))
